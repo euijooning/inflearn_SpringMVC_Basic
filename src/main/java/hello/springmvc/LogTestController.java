16c7e936976e92ba76c6803185b040c09d5015e4
EUIJOON <euijoonism@gmail.com>
2023-09-03 20:40:21 +0900

    로그와 로깅 간단히 알아보기

---
+++ src/main/java/hello/springmvc/LogTestController.java
@@ -0,0 +1,27 @@
+package hello.springmvc;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RestController //이 어노테이션이 뷰 이름으로 랜더링을 안 하게 만들어줌
+public class LogTestController {
+//    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info(" info log={}", name);
+        log.warn(" warn log={}", name);
+        log.error("error log={}", name);
+
+        return "ok";
+    }
+}
